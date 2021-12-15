8ae3497645422623c6dc51b89b00e4f15cbc9def
Jayanth Gudimella <gjaynir0508@gmail.com>
2021-12-15 19:03:00 +0530

    Add splash screen

---
+++ pages/splash.js
@@ -0,0 +1,7 @@
+export default function Splash() {
+	const style = {
+		margin: "auto",
+		transform: "translateY(20vh)",
+	};
+	return <img src="/images/splash.gif" alt="splash-screen" style={style} />;
+}
